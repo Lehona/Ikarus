637e33725cf5951ceb8d7ffa28129e177e43989e
szapp <dev.szapp@gmail.com>
2025-01-18 18:46:25 +0100

    Bump version number

--- Ikarus.d
+++ Ikarus.d
@@ -3,7 +3,7 @@
 //  Kern des Skriptpakets "Ikarus"
 //      Autor      : Sektenspinner
 //      Co-Autor   : Gottfried, mud-freak, Neconspictor
-//	    Version    : 1.2.2
+//	    Version    : 1.2.3
 //
 //######################################################
 
@@ -146,7 +146,7 @@
 //   that may have old versions, use this:
 //----------------------------------------------
 
-const int IKARUS_VERSION = 10202; //2 digits for Major and Minor Revision number.
+const int IKARUS_VERSION = 10203; //2 digits for Major and Minor Revision number.
 
 /* returns 1 if the version of Ikarus is the specified version or newer */
 func int MEM_CheckVersion(var int base, var int major, var int minor) {
